1f049a43c481cc0b9c4c7c6d52bb0e93e171efb6
rakesh77674 <rockychaudhary2222@gmail.com>
2022-04-27 16:46:53 +0545

    Populating data in frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('shop.index');
+        $product = Product::all();
+        return view('shop.index',compact('product'));
     }
 }
